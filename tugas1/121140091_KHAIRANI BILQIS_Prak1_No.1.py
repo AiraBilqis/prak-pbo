93da979b6d16c988c4772a2a5c338313b0dbad3e
AiraBilqis <126907709+AiraBilqis@users.noreply.github.com>
2023-03-04 01:42:01 +0700

    Create 121140091_KHAIRANI BILQIS_Prak1_No.1.py

---
+++ tugas1/121140091_KHAIRANI BILQIS_Prak1_No.1.py
@@ -0,0 +1,16 @@
+print("============================================")
+print("Nama  : KHAIRANI BILQIS")
+print("NIM   : 121140091")
+print("Kelas : RB")
+print("============================================")
+print("~~  Tugas Program Python Persegi Bintang  ~~")
+print("============================================")
+print()
+ 
+besar_persegi = int(input("Input besar persegi: "))
+print()
+ 
+for i in range(besar_persegi):
+  for j in range(besar_persegi):
+    print("* ",end="")
+  print()
