713e83e247551f8cb314388802438e3475e01709
AiraBilqis <126907709+AiraBilqis@users.noreply.github.com>
2023-03-04 01:42:29 +0700

    Add files via upload

---
+++ tugas1/121140091_KHAIRANI BILQIS_Prak1_No.2.py
@@ -0,0 +1,25 @@
+print("============================================")
+print("Nama  : KHAIRANI BILQIS")
+print("NIM   : 121140091")
+print("Kelas : RB")
+print("============================================")
+print("~~               LOGIN AKUN               ~~")
+print("============================================")
+
+username ="informatika"
+password ="12345678"
+def login (user_name,pass_word):
+    if user_name != username or pass_word != password:
+        print("Username atau Password salah coba lagi!")
+        print()
+    else:
+        print()
+        print("=========== ~~ LOGIN BERHASIL ~~ ===========")
+        exit()
+
+for i in range (0,3):
+    username_=input("Username Anda : ")
+    password_=input("Password Anda : ")
+    login (username_, password_)
+
+print("=========== !! AKUN TERBLOKIR !! ===========")
