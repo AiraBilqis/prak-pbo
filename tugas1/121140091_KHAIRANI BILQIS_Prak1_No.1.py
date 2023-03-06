4c422e8c1226d7deec914192932e4914f5e0795f
Khairani Bilqis <126907709+AiraBilqis@users.noreply.github.com>
2023-03-06 19:34:11 +0700

    Update 121140091_KHAIRANI BILQIS_Prak1_No.1.py

--- tugas1/121140091_KHAIRANI BILQIS_Prak1_No.1.py
+++ tugas1/121140091_KHAIRANI BILQIS_Prak1_No.1.py
@@ -6,10 +6,12 @@ print("============================================")
 print("~~  Tugas Program Python Persegi Bintang  ~~")
 print("============================================")
 print()
- 
+
+#minta inputan n
 besar_persegi = int(input("Input besar persegi: "))
 print()
- 
+
+#algoritma persegi dengan perulangan
 for i in range(besar_persegi):
   for j in range(besar_persegi):
     print("* ",end="")
