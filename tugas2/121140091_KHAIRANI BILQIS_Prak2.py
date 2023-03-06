ef82d8987f09b6fa9f8ddf6ab723406c27cbe927
Khairani Bilqis <126907709+AiraBilqis@users.noreply.github.com>
2023-03-06 19:30:15 +0700

    Update 121140091_KHAIRANI BILQIS_Prak2.py

--- tugas2/121140091_KHAIRANI BILQIS_Prak2.py
+++ tugas2/121140091_KHAIRANI BILQIS_Prak2.py
@@ -6,9 +6,11 @@ print("============================================")
 print("~~    Tugas Program Python Minggu Ke-2    ~~")
 print("============================================")
 print()
- 
+
+#class 
 class Mahasiswa:
-    def __init__(self, nama, nim, semester, kelas_pbo, sks, total_sks_saat_ini):
+    #konstruktor
+    def __init__(self, nama, nim, semester, kelas_pbo, sks, total_sks_saat_ini): #atribut objek
         self.nama = nama
         self.nim = nim
         self.semester = semester
@@ -16,6 +18,7 @@ class Mahasiswa:
         self.sks = sks
         self.total_sks_saat_ini = total_sks_saat_ini
 
+    #method
     def data_mahasiswa(self):
         print("Nama Mahasiswa          : ", self.nama)
         print("NIM Mahasiswa           : ", self.nim)
@@ -24,5 +27,6 @@ class Mahasiswa:
         print("Jumlah SKS Mahasiswa    : ", self.sks)
         print("SKS Transkip Mahasiswa  : ", self.total_sks_saat_ini)
 
+#objek
 data = Mahasiswa("Khairani Bilqis", "121140091", "4", "RB", "22", "61")
 data.data_mahasiswa()
