9464e1aa0fe24a94674648d475c94e0bfdd6ba0d
Khairani Bilqis <126907709+AiraBilqis@users.noreply.github.com>
2023-03-27 20:46:17 +0700

    Create 121140091_KHAIRANI BILQIS_Prak4_No2.py

---
+++ tugas4/121140091_KHAIRANI BILQIS_Prak4_No2.py
@@ -0,0 +1,10 @@
+print("============================================")
+print("Nama  : KHAIRANI BILQIS")
+print("NIM   : 121140091")
+print("Kelas : RB")
+print("============================================")
+print("~~      Tugas Python Minggu Ke-4 No.2     ~~")
+print("============================================")
+print()
+
+class
